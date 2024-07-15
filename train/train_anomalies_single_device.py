359cc0b983f34d889243df1280562a600af4daa1
Hee Kim <heekim1@gmail.com>
2024-07-15 00:02:51 -0700

    update models and train

--- train/train_anomalies_single_device.py
+++ train/train_anomalies_single_device.py
@@ -1,14 +1,18 @@
-import pandas as pd
-from sklearn.ensemble import IsolationForest
-from sklearn.svm import OneClassSVM
-from sklearn.preprocessing import StandardScaler
-from keras.models import Sequential
-from keras.layers import Input, Dense, LSTM, RepeatVector, TimeDistributed, Dropout
-from keras.optimizers import Adam
-import joblib
+# Function to train and evaluate the model
+import os,sys
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
-from tensorflow.keras.regularizers import l2
+
+# Add the airo_ml directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+# Import the models
+from models.autoencoder_model import AutoencoderModel
+from models.isolation_forest_model import IsolationForestModel
+from models.one_class_svm_model import OneClassSVMModel
+from models.lstm_autoencoder_model import LSTMAutoencoderModel
 
 # Check TensorFlow version and GPU availability
 print("TensorFlow version:", tf.__version__)
@@ -25,61 +29,40 @@ else:
     print("GPU is not available. Training will use CPU.")
 
 
-# Function to train and evaluate the model
-def train_model(imeis_str, batch_size, X, X_scaled):
-    """
+def train_model(imeis_str, batch_size, X, X_scaled, output="model"):
+
+    # Train Isolation Forest
     print("Train Isolation Forest")
-    isolation_forest = IsolationForest(contamination=0.01)
-    isolation_forest.fit(X_scaled)
-    joblib.dump(isolation_forest, f'isolation_forest_model_{imeis_str}_{batch_size}.pkl')
-
-
+    isolation_forest = IsolationForestModel(contamination=0.01)
+    isolation_forest.train(X_scaled, model_path=f'{output}/isolation_forest_model_{imeis_str}_{batch_size}.pkl')
+    """
+    # Train One-Class SVM
     print("Train One-Class SVM")
-    one_class_svm = OneClassSVM(nu=0.01, kernel="rbf", gamma=0.1)
-    one_class_svm.fit(X_scaled)
-    joblib.dump(one_class_svm, f'one_class_svm_model_{imeis_str}_{batch_size}.pkl')
+    one_class_svm = OneClassSVMModel(nu=0.01, kernel="rbf", gamma=0.1)
+    one_class_svm.train(X_scaled, model_path=f'{output}/one_class_svm_model_{imeis_str}_{batch_size}.pkl')
+    """
 
-
+    # Train Autoencoder
     print("Train Autoencoder")
     input_dim = X_scaled.shape[1]
-    encoding_dim = 14
-    hidden_dim = int(encoding_dim / 2)
-
-    autoencoder = Sequential()
-    autoencoder.add(Input(shape=(input_dim,)))
-    autoencoder.add(Dense(encoding_dim, activation="relu"))
-    autoencoder.add(Dense(hidden_dim, activation="relu"))
-    autoencoder.add(Dense(encoding_dim, activation="relu"))
-    autoencoder.add(Dense(input_dim, activation="sigmoid"))
-    autoencoder.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse')
-
-    history = autoencoder.fit(X_scaled, X_scaled, epochs=12, batch_size=batch_size, validation_split=0.1, verbose=1)
-    autoencoder.save(f'autoencoder_model_{imeis_str}_{batch_size}.h5')
+    autoencoder = AutoencoderModel(input_dim=input_dim)
+    history = autoencoder.train(X_scaled, batch_size, epochs=12)
+    autoencoder.save_model(f'{output}/autoencoder_model_{imeis_str}_{batch_size}.keras')
     print(f"Autoencoder - Batch Size: {batch_size}, Loss: {history.history['loss'][-1]}, Val Loss: {history.history['val_loss'][-1]}")
-    """
 
+    # Train LSTM-based model for time-series data
     print("Train LSTM-based model for time-series data")
     # Reshape data for LSTM model
     X_time_series = np.reshape(X_scaled, (X_scaled.shape[0], 1, X_scaled.shape[1]))
-    print("X_time_series shape:", X_time_series.shape)
     input_shape = (X_time_series.shape[1], X_time_series.shape[2])
-    print("input_shape:", input_shape)
 
-    lstm_autoencoder = Sequential()
-    lstm_autoencoder.add(Input(shape=input_shape))
-    lstm_autoencoder.add(LSTM(100, activation='relu', return_sequences=True))
-    lstm_autoencoder.add(LSTM(50, activation='relu', return_sequences=False))
-    lstm_autoencoder.add(RepeatVector(X_time_series.shape[1]))
-    lstm_autoencoder.add(LSTM(50, activation='relu', return_sequences=True))
-    lstm_autoencoder.add(LSTM(100, activation='relu', return_sequences=True))
-    lstm_autoencoder.add(TimeDistributed(Dense(X_time_series.shape[2])))
-    lstm_autoencoder.compile(optimizer='adam', loss='mse')
+    # Initialize and train the LSTM autoencoder model
+    lstm_autoencoder = LSTMAutoencoderModel(input_shape=input_shape)
+    history = lstm_autoencoder.train(X_time_series, batch_size, epochs=12)
+    lstm_autoencoder.save_model(f'{output}/lstm_autoencoder_model_{imeis_str}_{batch_size}.keras')
+
+    print(f"LSTM Autoencoder - Batch Size: {batch_size}, Loss: {history.history['loss'][-1]}, Val Loss: {history.history['val_loss'][-1]}")
     
-    lstm_history = lstm_autoencoder.fit(X_time_series, X_time_series, epochs=12, batch_size=batch_size, validation_split=0.1)
-    lstm_autoencoder.save(f'lstm_autoencoder_model_{imeis_str}_{batch_size}.h5')
-
-    print(f"LSTM Autoencoder - Batch Size: {batch_size}, Loss: {lstm_history.history['loss'][-1]}, Val Loss: {lstm_history.history['val_loss'][-1]}")
-
 
 devices = [
     '883543430497535',
@@ -104,7 +87,7 @@ for device in devices:
     imeis = df['imei'].unique()
     # Convert to string
     imeis_str = ', '.join(map(str, imeis))
-    print(f"training {imeis_str} ...")
+    print(f"Training models for {imeis_str} ...")
 
     # Feature selection and preprocessing
     features = ['sound_db', 'noise_db', 'breath_rate', 'heart_rate', 'temperature', 'humedity', 'time_in_minutes']
@@ -117,4 +100,4 @@ for device in devices:
     # Test different batch sizes
     for batch_size in [32]: #[32, 64, 128, 256]:
         print(f"Testing batch size: {batch_size}")
-        train_model(imeis_str, batch_size, X, X_scaled)
+        train_model(imeis_str, batch_size, X, X_scaled, output="/Users/hkim75/Airo/airo_ml/train/model_3_single_07132024")
